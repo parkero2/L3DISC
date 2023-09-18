14edc08b954f0d20c647cabf52a76ea4240c1e33
parkero2 <72895391+parkero2@users.noreply.github.com>
2023-09-18 16:02:27 +1200

    Add head dialogue formatted

--- TKWindows/addHead.py
+++ TKWindows/addHead.py
@@ -5,23 +5,32 @@ from tkinter import filedialog
 from tkinter import font
 import main as mainwindow
 
-headNameEntry, headTypeDropdown = None
+headNameEntry, headTypeDropdown = None, None
 
-def changedValue(dropDownMenu : ttk.Combobox):
+
+def changedValue(dropDownMenu: ttk.Combobox):
     global headNameEntry
     if (not dropDownMenu.get() == "NEW HEAD TYPE") and (not headNameEntry == None):
         headNameEntry.configure(state="disabled")
+
+
+def submitHead(rigsDB, selected_rig, addHeadWindow, headTypeDropdown,
+               headNameEntry, totalChannelsEntry, intensityChannelEntry,
+               redChannelEntry, greenChannelEntry, blueChannelEntry,
+               amberChannelEntry, whiteChannelEntry, panChannelEntry,
+               tiltChannelEntry, shutterChannelEntry):
+    pass
+
 
 def addHead(root, rigsDB, selected_rig):
     global headNameEntry, headTypeDropdown
     # Add head window
     addHeadWindow = tk.Toplevel(root)
     addHeadWindow.title("Add Head")
-    addHeadWindow.geometry("300x200")
     addHeadWindow.resizable(False, False)
 
     # Head addition form
-    #Get the existing head types
+    # Get the existing head types
     HeadTypes = f"SELECT DISTINCT HeadType FROM {selected_rig}"
     HeadTypeRows = rigsDB.execute(HeadTypes)
     HeadTypeRows = [row[0] for row in HeadTypeRows]
@@ -35,7 +44,8 @@ def addHead(root, rigsDB, selected_rig):
     headTypeLabel.grid(row=0, column=0, sticky="nw")
 
     headTypeDropdown = ttk.Combobox(addHeadWindow, values=HeadTypeRows)
-    headTypeDropdown.bind("<<ComboboxSelected>>", changedValue(headTypeDropdown))
+    headTypeDropdown.bind("<<ComboboxSelected>>",
+                          changedValue(headTypeDropdown))
     headTypeDropdown.grid(row=1, column=0, sticky="nw")
 
     # Head name
@@ -44,3 +54,85 @@ def addHead(root, rigsDB, selected_rig):
 
     headNameEntry = tk.Entry(addHeadWindow)
     headNameEntry.grid(row=3, column=0, sticky="nw")
+
+    # Total number of channels
+    totalChannelsLabel = tk.Label(addHeadWindow, text="Total Channels:")
+    totalChannelsLabel.grid(row=4, column=0, sticky="nw")
+
+    totalChannelsEntry = tk.Entry(addHeadWindow)
+    totalChannelsEntry.grid(row=5, column=0, sticky="nw")
+
+    # intensity channel
+    intensityChannelLabel = tk.Label(addHeadWindow, text="Intensity Channel:")
+    intensityChannelLabel.grid(row=6, column=0, sticky="nw")
+
+    intensityChannelEntry = tk.Entry(addHeadWindow)
+    intensityChannelEntry.grid(row=7, column=0, sticky="nw")
+
+    # Red channel
+    redChannelLabel = tk.Label(addHeadWindow, text="Red Channel:")
+    redChannelLabel.grid(row=8, column=0, sticky="nw")
+
+    redChannelEntry = tk.Entry(addHeadWindow)
+    redChannelEntry.grid(row=9, column=0, sticky="nw")
+
+    # Green channel
+    greenChannelLabel = tk.Label(addHeadWindow, text="Green Channel:")
+    greenChannelLabel.grid(row=10, column=0, sticky="nw")
+
+    greenChannelEntry = tk.Entry(addHeadWindow)
+    greenChannelEntry.grid(row=11, column=0, sticky="nw")
+
+    # Blue channel
+    blueChannelLabel = tk.Label(addHeadWindow, text="Blue Channel:")
+    blueChannelLabel.grid(row=12, column=0, sticky="nw")
+
+    blueChannelEntry = tk.Entry(addHeadWindow)
+    blueChannelEntry.grid(row=13, column=0, sticky="nw")
+
+    # Amber channel
+    amberChannelLabel = tk.Label(addHeadWindow, text="Amber Channel:")
+    amberChannelLabel.grid(row=16, column=0, sticky="nw")
+
+    amberChannelEntry = tk.Entry(addHeadWindow)
+    amberChannelEntry.grid(row=17, column=0, sticky="nw")
+
+    # White channel
+    whiteChannelLabel = tk.Label(addHeadWindow, text="White Channel:")
+    whiteChannelLabel.grid(row=14, column=0, sticky="nw")
+
+    whiteChannelEntry = tk.Entry(addHeadWindow)
+    whiteChannelEntry.grid(row=15, column=0, sticky="nw")
+
+    # Pan channel
+    panChannelLabel = tk.Label(addHeadWindow, text="Pan Channel:")
+    panChannelLabel.grid(row=18, column=0, sticky="nw")
+
+    panChannelEntry = tk.Entry(addHeadWindow)
+    panChannelEntry.grid(row=19, column=0, sticky="nw")
+
+    # Tilt channel
+    tiltChannelLabel = tk.Label(addHeadWindow, text="Tilt Channel:")
+    tiltChannelLabel.grid(row=20, column=0, sticky="nw")
+
+    tiltChannelEntry = tk.Entry(addHeadWindow)
+    tiltChannelEntry.grid(row=21, column=0, sticky="nw")
+
+    # Shutter channel
+    shutterChannelLabel = tk.Label(addHeadWindow, text="Shutter Channel:")
+    shutterChannelLabel.grid(row=22, column=0, sticky="nw")
+
+    shutterChannelEntry = tk.Entry(addHeadWindow)
+    shutterChannelEntry.grid(row=23, column=0, sticky="nw")
+
+    # Submit button
+    submitButton = tk.Button(addHeadWindow, text="Submit", command=lambda:
+                             submitHead(rigsDB, selected_rig, addHeadWindow, 
+                                        headTypeDropdown, headNameEntry, 
+                                        totalChannelsEntry, 
+                                        intensityChannelEntry, redChannelEntry, 
+                                        greenChannelEntry, blueChannelEntry, 
+                                        amberChannelEntry, whiteChannelEntry, 
+                                        panChannelEntry, tiltChannelEntry, 
+                                        shutterChannelEntry))
+    submitButton.grid(row=24, column=0, sticky="nw")
